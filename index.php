603d5d09ab516007307ab935623a7c46da49f023
EnigmaWeb <EnigmaWeb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-27 04:05:57 +0000

    modified index.php
    git-svn-id: https://plugins.svn.wordpress.org/wp-catalogue/trunk@718695 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 Plugin Name: WP Catalogue
 Plugin URI: http://www.wordpress.org/extend/plugins/wp-catalogue/
 Description: Display your products in an attractive and professional catalogue. It's easy to use, easy to customise, and lets you show off your products in style.
-Author: Enigma Digital
+Author: Maeve Lander
 Version: 1.4
-Author URI: http://www.enigmaweb.com.au/
+Author URI: http://www.maevelander.net
 */
 //creating db tables
 
